569d24831c564036e14d955992f244b81cb8b2e9
backend <backend@slidebnb.de>
2026-04-10 14:35:38 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "@/components/ui/sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -68,6 +69,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <SonnerToaster position="top-right" richColors />
       </QueryClientProvider>
     </AuthProvider>
   )
